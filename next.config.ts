5bb445876c2e89ff429ff51983eb4f8b24d7ebf3
nileshgarg-futred <nilesh@futred.com>
2025-03-02 16:12:07 +0530

    again

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: 'build',
+  // Ensure the following options are set
+  generateBuildId: async () => {
+    return 'my-build-id'; // You can use any unique identifier here
+  },
+  exportTrailingSlash: true,
   // Add any other necessary configuration options here
 };
 

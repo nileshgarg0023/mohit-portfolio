1ddd15fe63f35a38d18191038c7e42d91c7e25ee
nileshgarg-futred <nilesh@futred.com>
2025-03-02 19:31:26 +0530

    fixed error might work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
-import Image from "next/image";
+import Hero from '@/components/hero'
+import About from '@/components/about'
+import Experience from '@/components/experience'
+import Skills from '@/components/skills'
+import Projects from '@/components/projects'
+import Contact from '@/components/contact'
+import Footer from '@/components/footer'
 
 export default function Home() {
   return (

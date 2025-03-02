67135ef79ed6bb4637fe2804299b11f9d305d260
nileshgarg-futred <nilesh@futred.com>
2025-03-02 16:08:13 +0530

    test again

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   distDir: 'build',
+  // Add any other necessary configuration options here
 };
 
 export default nextConfig;
